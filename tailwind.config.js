e0f4f55835cbf3fcc7bf375a4cca3dfc41acd5b9
JanuaryClouds <janmichaelcristianbcruz@gmail.com>
2024-07-11 22:33:50 +0800

    Deploy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/user/*.{html}"],
+  theme: {
+    colors: {
+      'primary': '#8254BC',
+      'main-l-gray': '#F4F4F4',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
